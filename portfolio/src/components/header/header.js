6d328e447b65221f69ebf8564e97de35d9a478dd
Heather Runyon <hrocco25@gmail.COM>
2020-02-27 15:08:07 -0700

    styling for portfolio

--- portfolio/src/components/header/header.js
+++ portfolio/src/components/header/header.js
@@ -6,7 +6,7 @@ function Header(props) {
     return (
         <div className="header">
             <h1>Hi, I'm Heather Runyon!</h1>
-            <h2>A Full Stack Developer</h2>
+            <h2>Full Stack Developer</h2>
         </div>
     )
 }
