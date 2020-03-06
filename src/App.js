4a9cd14c362190a312b13c77c1848c60d946bb6f
Heather Runyon <hrocco25@gmail.COM>
2020-03-06 15:05:49 -0700

    red

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Helmet} from "react-helmet";
+import Helmet from "react-helmet";
 import Header from './components/header/header'
 import Contact from './components/contact/contact'
 import Projects from './components/projects/projects'
@@ -16,7 +16,13 @@ function App() {
           <meta charSet="UTF-8"/>
           <title>Heather Runyon</title>
           <meta name="description" content="Heather Runyon Portfolio"/>
-          <link rel="icon" type="image/png" href="favicon.ico" size="16x16" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+          <meta name="msapplication-TileColor" content="#da532c"/>
+          <meta name="theme-color" content="#ffffff"/>
         </Helmet>
       </div>
       <Nav />
