5ec5dfe3d1011b253071404652593257f7b0aa36
Heather Runyon <hrocco25@gmail.COM>
2020-03-06 13:51:14 -0700

    projects update

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -13,57 +13,61 @@ function Projects(props) {
             <div className="allProjects">
 
                 <div>
-                    <div className="trivia">
-                        <h3>Colorado Springs Trivia Game</h3>
-                        <a href="https://hrocco25.github.io/seir-project-one/" target="blank">
-                            <img src={trivia} alt="trivia"/>
-                        </a>
-                        <p>Responsive trivia game about Colorado Springs that uses HTLM, CSS and JavaScript.</p> 
-                    </div>
+                    <div className="camping">
+                        <a href="https://dispersed-camping.herokuapp.com/" target="_blank">
+                            <h3>Free Camping App</h3>
+                            <img src={camping} alt="camping" /> 
+                            <p>This is a full stack app created with <b>Django</b>, <b>Python</b>, <b>CSS</b>, <b>Bootstrap</b>, and <b>AWS</b>.  You can look up and add free campsites to the site.</p>   
+                        </a>     
+                    </div>         
                     <div className="github">
-                        <a href="https://github.com/hrocco25/seir-project-one" target="blank"><h4>Check out my code on Github</h4></a>
+                        <a href="https://github.com/hrocco25/camping" target="_blank"><h4>Check out my code on Github</h4></a>
                     </div>
                 </div>
 
                 <div>
                     <div className="brewery">
-                        <h3>Brewery Search App</h3> 
-                        <a href="https://heatherbreweryapp2.herokuapp.com/" target="blank">
+                        <a href="https://heatherbreweryapp2.herokuapp.com/" target="_blank">
+                            <h3>Brewery Search App</h3> 
                             <img src={brewery} alt="brewery" />
+                            <p>React app that utilize an API to get data on breweries in the USA. You can search for a brewery by its name or location.  This app was created with <b>React</b>, <b>JavaScript</b>, <b>CSS</b>, and <b>HTML</b>. </p>      
+
                         </a>
-                        <p>React app that utilize an API to get data on breweries in the USA. You can search for a brewery by its name or location.  This app was created with React, JavaScript, CSS, and HTML. </p>      
                     </div>
                     <div className="github">
-                        <a href="https://github.com/hrocco25/brewery" target="blank"><h4>Check out my code on Github</h4></a>
+                        <a href="https://github.com/hrocco25/brewery" target="_blank"><h4>Check out my code on Github</h4></a>
                     </div>
                 </div>
-
+                
                 <div>
                     <div className="greatReads">
-                        <h3>Great Reads App</h3>
-                        <a href="https://greatreads.netlify.com/" target="blank">
+                        <a href="https://greatreads.netlify.com/" target="_blank">
+                            <h3>Great Reads App</h3>
                             <img src={read} alt="greatreads"/>
+                            <p>For this project, I had the pleasure of working with some of my classmates at General Assembly. We created a full stack React app with full CRUD.  It was designed with <b>React</b>, <b>CSS</b>, <b>HTML</b>, <b>JavaScript</b>, and <b>Express</b>.</p>
                         </a>
-                        <p>For this project, I had the pleasure of working with some of my classmates at General Assembly. We created a full stack React app with full CRUD.  It was designed with React, CSS, HTML, JavaScript, and Express.</p>
                     </div>
                     <div className="github">
-                        <a href="https://github.com/viviRbi/Great-reads" target="blank"><h4>Check out my code on Github</h4></a>
+                        <a href="https://github.com/viviRbi/Great-reads" target="_blank"><h4>Check out my code on Github</h4></a>
                     </div>
                 </div>
+
 
                 <div>
-                    <div className="camping">
-                        <h3>Free Camping App</h3>
-                        <a href="https://dispersed-camping.herokuapp.com/" target="blank">
-                            <img src={camping} alt="camping" />    
-                        </a>     
-                        <p>This is a full stack app created with Django, Python, CSS, Bootstrap, and AWS.  You can look up and add free campsites to the site.</p>
-
-                    </div>         
+                    <div className="trivia">
+                        <a href="https://hrocco25.github.io/seir-project-one/" target="_blank">
+                            <h3>Colorado Springs Trivia Game</h3>
+                            <img src={trivia} alt="trivia"/>
+                            <p>Responsive trivia game about Colorado Springs that uses <b>HTML</b>, <b>CSS</b> and <b>JavaScript</b>.</p> 
+                        </a>
+                    </div>
                     <div className="github">
-                        <a href="https://github.com/hrocco25/camping" target="blank"><h4>Check out my code on Github</h4></a>
+                        <a href="https://github.com/hrocco25/seir-project-one" target="_blank"><h4>Check out my code on Github</h4></a>
                     </div>
                 </div>
+
+        
+              
 
             </div>
             
