2fd862171147bab738451b854d35b1bfb42c02a7
Heather Runyon <hrocco25@gmail.COM>
2020-03-06 19:03:22 -0700

    resume

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -10,7 +10,7 @@ function Contact(props) {
             <h1>Lets get in touch!</h1>
             <div className="email">
                 <h4>Email me @ hrocco25@gmail.com</h4>
-                <a href="https://drive.google.com/open?id=1sFSbQOjLTfKiFUddv7N9dC0sdUJRabwy" target="_blank">View my Resume</a>
+                <a href="https://drive.google.com/open?id=1PH2114ImSZuHE4hIBUb31Ta3c6olZtmu" target="_blank">View my Resume</a>
             </div>
             <div className="contactIcon">
                 <a href="https://github.com/hrocco25" target="blank">
